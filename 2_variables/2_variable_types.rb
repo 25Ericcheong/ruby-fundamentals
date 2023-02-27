f6a712d5b6c921841b4e29d455b45e052ec1f8be
25Ericcheong <ericcheongkaikit@gmail.com>
2023-02-27 21:12:26 +1000

    variable types

---
+++ 2_variables/2_variable_types.rb
@@ -0,0 +1,19 @@
+# constants
+# can be changed in ruby
+CONSTANTS_YES = "something" 
+
+
+# global variables
+$var = "everywhere"
+
+
+# class variables
+@@class_var = "class stuff"
+
+
+# instance variables
+@instance_var = "instance stuff"
+
+
+# local variables
+var = "some local variables"
