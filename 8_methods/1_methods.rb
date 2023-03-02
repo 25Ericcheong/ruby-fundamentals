018a74ed9f9b81d252bc5a29400160ef3958fada
25Ericcheong <ericcheongkaikit@gmail.com>
2023-03-02 21:38:51 +1000

    quirky methods

--- 8_methods/1_methods.rb
+++ 8_methods/1_methods.rb
@@ -9,3 +9,21 @@ def explicit_my_name
 end
   
 puts my_name 
+
+
+
+def even_odd(number)
+    # removing the return keyword here will be detrimental as Ruby will continue on to execute the last expression of this method
+    unless number.is_a? Numeric
+        return "A number was not entered."
+    end
+
+    if number % 2 == 0
+        "That is an even number."
+    else
+        "That is an odd number."
+    end
+end
+
+puts even_odd(20)
+puts even_odd("Ruby")
